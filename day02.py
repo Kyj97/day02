f6d03ee4cc66306da2d26625951ce0028084d4aa
Kyj97 <gld66777@gmail.com>
2023-01-13 16:27:33 +0900

    last

--- day02.py
+++ day02.py
@@ -3,8 +3,8 @@ import random
 #4.1
 secret = random.randint(1, 10)
 guess = random.randint(1, 10)
-print(secret)
-print(guess)
+print(f'secret는 {secret} 입니다.')
+print(f'guess는 {guess} 입니다.')
 
 if secret > guess:
     print("too low")
@@ -12,17 +12,3 @@ elif secret == guess:
     print("just right")
 else:
     print("too high")
-
-#4.2
-small = False
-green = True
-if small:
-    if green:
-         print("완두콩은 작고 녹색이다.")
-    else:
-         print("체리는 작고 녹색이 아니다.")
-else:
-    if green:
-            print("수박은 크고 녹색이다.")
-    else:
-        print("호박은 크고 녹색이 아니다.")
---
+++ day02_4.2.py
@@ -0,0 +1,14 @@
+#4.2
+small = False
+green = False
+
+if small:
+    if green:
+         print("완두콩은 작고 녹색이다.")
+    else:
+         print("체리는 작고 녹색이 아니다.")
+else:
+    if green:
+            print("수박은 크고 녹색이다.")
+    else:
+        print("호박은 크고 녹색이 아니다.")
