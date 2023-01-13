46b3cef660f410137d240491b838e9dfcde51c34
Kyj97 <gld66777@gmail.com>
2023-01-13 16:15:36 +0900

    4.2

--- day02.py
+++ day02.py
@@ -1,5 +1,6 @@
 import random
 
+#4.1
 secret = random.randint(1, 10)
 guess = random.randint(1, 10)
 print(secret)
@@ -11,3 +12,17 @@ elif secret == guess:
     print("just right")
 else:
     print("too high")
+
+#4.2
+small = False
+green = True
+if small:
+    if green:
+         print("완두콩은 작고 녹색이다.")
+    else:
+         print("체리는 작고 녹색이 아니다.")
+else:
+    if green:
+            print("수박은 크고 녹색이다.")
+    else:
+        print("호박은 크고 녹색이 아니다.")
